dfe0c8e20ac9e7fd87ba1810311e84852aca3022
mo7m7d1 <mo7m7d1@gmail.com>
2023-04-06 01:10:58 +0300

    add: edit answers page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,10 @@ export default function Footer() {
 		<div className="flex justify-center items-center w-full absolute bottom-4">
 			<span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
 				© {new Date().getFullYear()}{" "}
-				<a href="" className="hover:underline hover:text-main_color">
+				<a
+					href="https://twitter.com/Mo7m7d1"
+					className="hover:underline hover:text-blue-500"
+				>
 					ALMO™
 				</a>
 				. All Rights Reserved.
